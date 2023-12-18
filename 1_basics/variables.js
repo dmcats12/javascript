7d6d034269746362317a4c8073273a6282e1ca9f
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-18 07:02:54 +0000

    varibles

---
+++ 1_basics/variables.js
@@ -0,0 +1,16 @@
+const Account_id=143567
+let AccountEmail="hitesh@gmail.com"
+var Accountpassword="12345"
+accountcity="delhi"
+
+
+
+//we cannot change the const valur because it is fixed//
+//let can be changed if want since it is a variable 
+//var is not used in javascript  because it didn't have the concept of scop {} we can't use it any particular area it will be changed in all area//
+
+
+accountEmail="hitsh2GOOGLE.COM"
+accountcityh="newyork"
+
+console.table([Account_id,AccountEmail,Accountpassword,accountcity])
