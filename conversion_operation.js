409450c9752a23bf7c5ab188b6849149ab3eae64
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-19 07:37:16 +0000

    operaters

--- conversion_operation.js
+++ conversion_operation.js
@@ -25,3 +25,10 @@ let somenumber=33
 let stringnumber=String(somenumber)
 console.log(stringnumber)
 console.log(typeof stringnumber)
+
+//********************operaters***************//
+//+//
+//-//
+//*//
+//"/"//
+//%//
