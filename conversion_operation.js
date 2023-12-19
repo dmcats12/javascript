c8772e02ae80baefcd161217f6b2015f5478b05c
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-19 07:31:42 +0000

    conversion

---
+++ conversion_operation.js
@@ -0,0 +1,27 @@
+const score="33abc"
+console.log(typeof(score))
+
+
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber)
+
+console.log(valueInnumber)
+
+// we have to do conevrsions in js if the input is not in desired form 
+// when a number "33"=>33 number
+// when we take "33abc"=> Nan
+//true=>1; false=>0;
+
+
+let Isloggedin=1
+let booleanIsloggedin=Boolean(Isloggedin)
+console.log(booleanIsloggedin)
+
+//1=true, 0=false 
+// ""=false ,"j"=true
+
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
