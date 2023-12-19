40b996858efe029fc4731402b15afa4d811c5772
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-19 07:58:28 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,12 @@
+//primitive datatypes//
+// string , Number , Boolean , null , undefined ,Symbol, Big int
+//refence(non primitive)
+// Array , objects , Functions
+
+const score = 100
+const scoreval=100.3
+const Isloggedin=false
+const outsidetemp=null      // primitive datatypes
+
+const id=Symbol('123')
+const idn=Symbol('123')
