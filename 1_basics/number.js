ead10aec16db4562d0ce66dabb6c64b3f820a1d0
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-23 08:16:22 +0000

    commit

---
+++ 1_basics/number.js
@@ -0,0 +1,5 @@
+const score=400
+console.log(score)
+
+const newscore= new Number(100)
+console.log(newscore)
---
+++ 1_basics/string.js
@@ -0,0 +1,9 @@
+const name= new String(`dhruv-mahajan`)
+console.log(name[0])
+
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+
+const newString=name.substring(0,4)
+console.log(newString)
