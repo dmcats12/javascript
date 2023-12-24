5e8149eb2b320da00fb4a10d986bb2e5ee0fbf7d
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-24 16:05:39 +0000

    b

---
+++ 1_basics/functions.js
@@ -0,0 +1,10 @@
+function addtwonum(number1,number2){// this is called parameters 
+   let result= number1+number2
+   return result
+   console.log("hitesh")// this will not be printed
+}
+const result=addtwonum(4,3)// this is called arguments
+
+console.log("result:" , result)
+
+
