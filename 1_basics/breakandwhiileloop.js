97f08c6a2b80c7a7e1849a7a7b5f613056ee702c
Dhruv mahajan <145915665+dmcats12@users.noreply.github.com>
2023-12-29 18:45:45 +0000

    commit

---
+++ 1_basics/breakandwhiileloop.js
@@ -0,0 +1,33 @@
+// for //
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (Element==5){
+//         "5 is the best number"
+//     }
+//     console.log(element);
+// }
+
+// first we intialize 
+//then check
+//and then increase it by one 
+let index=0
+while(index<=10){
+    console.log(`value of index is ${index}`);
+    index=index+2
+}
+let myArray=['flash','superman','batman']
+let arr=0;
+while (arr<myArray.length) {
+    console.log(`value is${myArray[arr]}`);
+    arr=arr+1
+}
+
+// loops to put on array 
+
+const arrr =[1,2,3,4,5]
+for (const num of arrr) {
+    console.log(num);
+}
+// map 
+const map=new Map
---
+++ 1_basics/controllflow.js
@@ -0,0 +1,11 @@
+// if 
+const IsUserLoggedIN=true
+const temp=7
+if(temp<10){
+    console.log("winter")
+}
+else{
+    console.log("there is time in winter")
+}
+
+// the scope case will also act here 
